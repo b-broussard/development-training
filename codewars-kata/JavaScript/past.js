7d6f146051d685c00c6c20a9c39caf48d9decc00
Brock Broussard <bbroussard@protonmail.com>
2020-02-12 23:07:59 -0600

    add past.js as solution for 2/10/2020

---
+++ codewars-kata/JavaScript/past.js
@@ -0,0 +1,8 @@
+/** 
+ * Taking a small break from kata ranked 5 or higher, this simply returns elapsed
+ * time in milliseconds.
+ */
+
+function past(h, m, s){
+  return (h * 60000 * 60) + m * 60000 + s * 1000;
+}
